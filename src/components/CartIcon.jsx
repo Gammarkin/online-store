b9b58fbcd13e50758f22831385eac832c2659b6c
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-05-02 15:30:16 -0300

    tried to fix routes again

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
-import Shopcart from '../pages/Shopcart';
 
 export default class CartIcon extends React.Component {
   render() {
     const {quantity} = this.props;
     return (
-      <Link
-        to="/shopcart"
-        Component={Shopcart}
-        data-testid="shopping-cart-button"
-      >
+      <Link to="/shopcart" data-testid="shopping-cart-button">
         <div className="shop_cart_icon">
           <img
             src="https://svgsilh.com/svg/294547.svg"
